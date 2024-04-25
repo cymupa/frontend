8dee6c7318afafc4f343c8cb36526efd994cbace
Semyon Okulov <67464545+scffs@users.noreply.github.com>
2024-04-26 02:45:41 +0700

    add pluginPurgeCss

--- components.d.ts
+++ components.d.ts
@@ -8,10 +8,10 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     Avatar: typeof import('primevue/avatar')['default']
-    Button: (typeof import('primevue/button'))['default']
+    Button: typeof import('primevue/button')['default']
     Card: typeof import('primevue/card')['default']
-    InputText: (typeof import('primevue/inputtext'))['default']
-    Password: (typeof import('primevue/password'))['default']
+    InputText: typeof import('primevue/inputtext')['default']
+    Password: typeof import('primevue/password')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     ScrollPanel: typeof import('primevue/scrollpanel')['default']
