f8bd5bd94fbcb3fde7e4fb8b391df3f4f6b901f8
Semyon Okulov <67464545+scffs@users.noreply.github.com>
2024-05-10 00:25:09 +0700

    wip: blanks for products

--- src/layout/Header/CartButton/CartButton.vue
+++ src/layout/Header/CartButton/CartButton.vue
@@ -16,7 +16,7 @@ const toggle = (event: MouseEvent) => {
     <button class="p-link layout-menu-button layout-topbar-button" @click="toggle">
       <i v-if="members.length" v-badge="members.length + 5" class="pi pi-shopping-cart"></i>
       <i v-else class="pi pi-shopping-cart"></i>
-      <span>Уведомления</span>
+      <span>Корзина</span>
     </button>
   </div>
 </template>
