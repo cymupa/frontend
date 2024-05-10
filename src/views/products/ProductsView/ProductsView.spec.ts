6aa563bf36270b3f9b5803927d4fbaab063fa093
okulov-sm <khidey@inbox.ru>
2024-05-10 16:22:32 +0700

    update test

--- src/views/products/ProductsView/ProductsView.spec.ts
+++ src/views/products/ProductsView/ProductsView.spec.ts
@@ -2,13 +2,19 @@ import { describe, expect, it } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 
+import {plugins} from "@/tests/plugins";
+
 import ProductsView from './ProductsView.vue'
 
 describe('HomeView', () => {
   it('renders properly', () => {
-    const wrapper = mount(ProductsView)
+    const wrapper = mount(ProductsView, {
+      global: {
+        plugins
+      }
+    })
 
     expect(wrapper.exists()).toBe(true)
-    // expect(wrapper.text()).toContain('Новости')
+    expect(wrapper.text()).toContain('Список товаров')
   })
 })
